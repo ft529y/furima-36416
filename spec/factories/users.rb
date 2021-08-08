a6592dd10152dbbb2a940536f50fda1748d49520
ft529y <yuto529reds@gmail.com>
2021-08-08 19:38:45 +0900

    FactoryBot生成

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@test'}
+    password              {'a1a1a1'}
+    password_confirmation {password}
+    last_name             {'太郎'}
+    first_name            {'山田'}
+    last_name_kana        {'タロウ'}
+    first_name_kana       {'ヤマダ'}
+    birthday              {'2000-01-01'}
+
+  end
+end
