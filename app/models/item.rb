ea6a421c7204c819ac2cdaf314d403611c4511e8
ft529y <yuto529reds@gmail.com>
2021-08-09 18:34:56 +0900

    itemモデル アソシエーション、バリデーション記述

---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :selling_price
+  end
+
+  validates :description, length: { maximum: 1000 }
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :are_id
+    validates
+  end
+end
