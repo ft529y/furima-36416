5d300b3a51482d5aacdd28b2d4050543b3f39152
ft529y <yuto529reds@gmail.com>
2021-08-09 16:14:44 +0900

    遷移先を分ける

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in?, except: [:index]
+
+  def index
+  end
+
+
+  private
+
+
+  def move_to_signed_in?
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
