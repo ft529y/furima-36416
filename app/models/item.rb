c229238bcbfdd35332a79aeb9a0a9364a549898b
ft529y <yuto529reds@gmail.com>
2021-08-10 19:24:26 +0900

    with_optionsでまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,17 +9,12 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :days_to_ship
 
-  validates :product_name, length: {maximum: 40}
-  validates :description, length: { maximum: 1000 }
-  
-  with_options presence:true, format: { with: /\A[0-9]+\z/ } do
-    validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
-  end
 
   with_options presence: true do
-    validates :product_name
-    validates :description
+    validates :product_name, length: {maximum: 40}
+    validates :description, length: { maximum: 1000 }
     validates :image
+    validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
