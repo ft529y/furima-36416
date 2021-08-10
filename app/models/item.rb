e26c2fd3ab4ad579a3692e3bab39949a1fe12c84
ft529y <yuto529reds@gmail.com>
2021-08-10 14:22:44 +0900

    itemモデル　制限設定の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,19 +9,25 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :days_to_ship
 
+  validates :product_name, length: {maximum: 40}
+  validates :description, length: { maximum: 1000 }
+  
+  with_options presence:true, format: { with: /\A[0-9]+\z/ } do
+    validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
+  end
+
   with_options presence: true do
     validates :product_name
     validates :description
-    validates :selling_price
+    validates :image
   end
-  validates :product_name, length: {maximum: 40}
-  validates :description, length: { maximum: 1000 }
 
-  with_options numericality: { other_than: 1, message: "can't be blank"} do
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
     validates :burden_id
-    validates :are_id
-    validates
+    validates :area_id
+    validates :days_to_ship_id
   end
+
 end
