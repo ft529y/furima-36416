8edb43bfe9b2c349c5d81917c902355dfe579320
ft529y <yuto529reds@gmail.com>
2021-08-10 14:23:24 +0900

    アクション、ストロングパラメーター記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 
   private
 
@@ -15,6 +28,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:product_name, :description, :category_id, :condition_id, :burden_id, :area_id, :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 end
