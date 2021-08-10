3914d71bf6baeb80fb1c7505f520f82df3b77649
ft529y <yuto529reds@gmail.com>
2021-08-10 18:00:50 +0900

    itemモデル単体テストコード記述

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    product_name       { 'test' }
+    description        { Faker::Lorem.sentence }
+    category_id        { "2" }
+    condition_id       { "2" }
+    burden_id          { "2" }
+    area_id            { "2" }
+    days_to_ship_id    { "2" }
+    selling_price      { 3500 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
