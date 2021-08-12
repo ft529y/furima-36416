bdd98f3f560c41ee49db22db5574446d1e2e30e2
ft529y <yuto529reds@gmail.com>
2021-08-12 17:25:53 +0900

    ユーザー認証　記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,18 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :check_purchase_user, only: :index
+
 
   def index
     @item = Item.find(params[:item_id])
+  end
+
+
+  private
+  def check_purchase_user
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
