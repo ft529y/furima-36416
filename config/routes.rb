5822a03b06cb2e6d52f3b8789d03863fda8dec9d
ft529y <yuto529reds@gmail.com>
2021-08-12 21:53:32 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :purchases, only: :index
+    resources :purchases, only: [:index, :create]
   end
 end
