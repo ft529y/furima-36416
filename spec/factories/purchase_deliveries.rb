17f33a1edc6e910fd8cd3fab7371847a26c90039
ft529y <yuto529reds@gmail.com>
2021-08-13 21:41:49 +0900

    purchase_deliveryのFactoryBot及び単体テストコード記述

---
+++ spec/factories/purchase_deliveries.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_delivery do
+    postal_code        {'123-4567'}
+    phone_number       {'01234567890'}
+    municipality       {'東京都港区'}
+    address            {'青山1-1-1'}
+    building_name      {'サンプルビル100'}
+    prefecture_id      {2}
+    token              {"tok_abcdefghijk00000000000000000"}
+  end
+end
