d7c798134d75e08e86818f3b4053b7e7d7418cde
ft529y <yuto529reds@gmail.com>
2021-08-13 16:13:55 +0900

    purchaseコントローラー記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,21 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase_delivery = PurchaseDelivery.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      pay_item
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+    
+    
   end
 
 
@@ -14,5 +29,18 @@ class PurchasesController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.selling_price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def purchase_params
+    params.require(:purchase_delivery).permit(:postal_code, :phone_number, :municipality, :address, :building_name, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
