a7577bcf583e31fec331ebb6e696db3c490f53e3
ft529y <yuto529reds@gmail.com>
2021-08-14 13:29:41 +0900

    purchase_delivery単体テストコード修正

--- spec/models/purchase_delivery_spec.rb
+++ spec/models/purchase_delivery_spec.rb
@@ -75,6 +75,16 @@ RSpec.describe PurchaseDelivery, type: :model do
         @purchase_delivery.valid?
         expect(@purchase_delivery.errors.full_messages).to include('Phone number is invalid')
       end
+      it 'user_idが空だと登録できない' do
+        @purchase_delivery.user_id = nil
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと登録できない' do
+        @purchase_delivery.item_id = nil
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
