5e1d9d2de41f6d737bf1f26c68f09d9964f0f207
ft529y <yuto529reds@gmail.com>
2021-08-18 19:42:55 +0900

    likesコントローラー修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,21 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :create, :destroy]
+  before_action :set_item_params, only: [:create, :destroy]
+
   def create
-    @item = Item.find(params[:item_id])
     Like.create(user_id: current_user.id, item_id: @item.id)
     redirect_to root_path
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
     @like = @item.likes.find(params[:id])
     @like.destroy
     redirect_to root_path
   end
+end
+
+private
+
+def set_item_params
+  @item = Item.find(params[:item_id])
 end
